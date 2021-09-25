329de81f992e8e2b912cf93b77909619d966ee11
qianiaoo <adnimm@163.com>
2021-09-25 23:52:25 +0900

    https://hcunovelgame.yude.jp

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import http from '../utils/http'
 
 
-export const hpImgPath = 'https://47.101.190.44:80/hp'
-export const baseUrl = 'https://47.101.190.44:80'
+export const hpImgPath = 'https://hcunovelgame.yude.jp/hp'
+export const baseUrl = 'https://hcunovelgame.yude.jp'
 
 export const getBanners = () => http.get('/hp/banner');
 
--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 import qs from 'qs'
 
-const django_host = 'https://47.101.190.44:80'
+const django_host = 'https://hcunovelgame.yude.jp'
 
 axios.interceptors.request.use(config => {
     // loading
